9511e33d74f44b4d73ac26f509c7a798cb4baa9a
REGINA_GREIBUVIENE <r.greibuviene@gmail.com>
2024-07-15 14:36:48 +0300

    Add @BeforEach cleanUpDatabase to RegistrationTests

--- API/src/test/java/RegistrationTests.java
+++ API/src/test/java/RegistrationTests.java
@@ -1,9 +1,28 @@
 import io.restassured.RestAssured;
 import io.restassured.http.ContentType;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+
 import static io.restassured.RestAssured.given;
 
 public class RegistrationTests {
+    @BeforeEach
+    void cleanUpDatabase() throws SQLException {
+        Connection connection = DriverManager.getConnection(
+                "jdbc:h2:tcp://localhost/~/recipe-sharing-platform/backend/rsp", "sa", "");
+        String deleteQuery = "DELETE FROM users_roles; DELETE FROM users;";
+
+        try (PreparedStatement preparedStatement = connection.prepareStatement(deleteQuery)) {
+            preparedStatement.executeUpdate();
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+    }
 
     @Test
     void whenVisitorRegistersSuccessfully_thenReturn201AndResponseBody() {
@@ -31,6 +50,8 @@ public class RegistrationTests {
                 .then()
                 .assertThat()
                 .statusCode(201);
+
     }
 }
+
 
