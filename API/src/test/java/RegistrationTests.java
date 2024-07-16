e887958249a17d06ff84aace64ab45885471da1d
REGINA_GREIBUVIENE <r.greibuviene@gmail.com>
2024-07-16 15:26:32 +0300

    Add parametrized Registration test for Gender and gender.csv file

--- API/src/test/java/RegistrationTests.java
+++ API/src/test/java/RegistrationTests.java
@@ -4,6 +4,8 @@ import org.hamcrest.Matcher;
 import org.hamcrest.Matchers;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvFileSource;
 
 import java.sql.Connection;
 import java.sql.DriverManager;
@@ -141,6 +143,77 @@ public class RegistrationTests {
                         equalTo("RasaRasiene"),
                         "gender",
                         equalTo("Other"),
+                        "dateOfBirth",
+                        equalTo("1980-09-25"),
+                        "email",
+                        equalTo("rasa@rasiene.lt"),
+                        "roles",
+                        hasSize(2),
+                        "roles[0].id",
+                        equalTo(1),
+                        "roles[1].id",
+                        equalTo(2),
+                        "authorities",
+                        hasSize(2),
+                        "authorities[0].id",
+                        equalTo(1),
+                        "authorities[1].id",
+                        equalTo(2),
+                        "username",
+                        equalTo("rasa@rasiene.lt"),
+                        "accountNonLocked",
+                        equalTo(true),
+                        "accountNonExpired",
+                        equalTo(true),
+                        "credentialsNonExpired",
+                        equalTo(true),
+                        "enabled",
+                        equalTo(true)
+                );
+    }
+    @ParameterizedTest
+    @CsvFileSource(resources = "/genders.csv")
+    void whenVisitorRegistersWithTwoRolesAndGender_thenReturn201AndResponseBody(String gender) {
+        given()
+                .body(
+                        """
+                {
+                    "lastName": "Rasiene",
+                    "firstName": "Rasa",
+                    "country": "Lithuania",
+                    "password": "Testas1*",
+                    "displayName": "RasaRasiene",
+                    "gender": "%s",
+                    "roles": [
+                        {"id": 1},
+                        {"id": 2}
+                    ],
+                    "dateOfBirth": "1980-09-25",
+                    "email": "rasa@rasiene.lt"
+                }
+                """
+                                .formatted(gender))
+                .contentType(ContentType.JSON)
+                .when()
+                .request("POST", "/register")
+                .then()
+                .assertThat()
+                .statusCode(201)
+                .body(
+                        "id",
+                        not(equalTo(0)),
+                        "firstName",
+                        equalTo("Rasa"),
+                        "lastName",
+                        equalTo("Rasiene"),
+                        "country",
+                        equalTo("Lithuania"),
+                        "password",
+                        not(equalTo("Testas1*")),
+                        "displayName",
+                        equalTo("RasaRasiene"),
+                        "gender",
+                        equalTo(gender),
                         "dateOfBirth",
                         equalTo("1980-09-25"),
                         "email",
