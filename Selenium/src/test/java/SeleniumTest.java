8146dcde1fe126f5f4d3a1deac47130ad44e15f5
REGINA_GREIBUVIENE <r.greibuviene@gmail.com>
2024-07-22 09:43:17 +0300

    Add H2 dependency to pom.xml and add Test for redirection from Home page to Registration form after click Register

--- Selenium/src/test/java/SeleniumTest.java
+++ Selenium/src/test/java/SeleniumTest.java
@@ -1,30 +1,44 @@
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.openqa.selenium.By;
+import org.openqa.selenium.JavascriptExecutor;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
 import java.time.Duration;
 
+import static java.lang.Thread.sleep;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class SeleniumTest {
     WebDriver driver;
 
     @BeforeEach
-    void setup() {
+    void cleanUpDatabase() throws SQLException {
+        Connection connection = DriverManager.getConnection(
+                "jdbc:h2:tcp://localhost/~/recipe-sharing-platform/backend/rsp", "sa", "");
+        String deleteQuery = "DELETE FROM users_roles; DELETE FROM users;";
+
+        try (PreparedStatement preparedStatement = connection.prepareStatement(deleteQuery)) {
+            preparedStatement.executeUpdate();
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
         driver = new ChromeDriver();
         driver.manage().window().maximize();
         driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(5));
-        driver.get("https://testpages.herokuapp.com/styled/calculator");
+        driver.get("http://localhost:5173");
     }
-    @Test
-    public void testCalculation(){
-        driver.findElement(By.id("number1")).sendKeys("3");
-        driver.findElement(By.cssSelector("#function > option:nth-child(3)")).click();
-        driver.findElement(By.id("number2")).sendKeys("2");
-        driver.findElement(By.id("calculate")).click();
 
-        assertEquals("1" ,driver.findElement(By.id("answer")).getText(), "Answer should be the same " );
+    @Test
+    public void testUserIsRedirectedFromHomePageToRegistrationFormAfterClickRegister() {
+        driver.findElement(By.cssSelector("#navbarSupportedContent > ul > li:nth-child(2) > a")).click();
+        assertEquals("http://localhost:5173/register", driver.getCurrentUrl(), "URL should be the same");
+
     }
 }
