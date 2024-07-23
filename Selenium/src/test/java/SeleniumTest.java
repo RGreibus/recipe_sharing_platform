81aa5d541fdfef52a9e1ccf3932654e4e7a61813
REGINA_GREIBUVIENE <r.greibuviene@gmail.com>
2024-07-23 10:45:16 +0300

    Add Registration test with too long Lastname

--- Selenium/src/test/java/SeleniumTest.java
+++ Selenium/src/test/java/SeleniumTest.java
@@ -279,4 +279,30 @@ public void testRegistrationWithFirstNameEmptyField() throws InterruptedExceptio
         WebElement errorMessage = driver.findElement(By.cssSelector("div.text-danger"));
         assertEquals("Minimum symbols: 2", errorMessage.getText(), "Error message should match");
     }
+    @Test
+    public void testRegistrationWithTooLongLastName() throws InterruptedException {
+        driver.findElement(By.cssSelector("#navbarSupportedContent > ul > li:nth-child(2) > a")).click();
+        driver.findElement(By.id("first-name")).sendKeys("Rasa");
+        driver.findElement(By.id("last-name")).sendKeys("Eldranorithanovelimustorianthropelmindoravianthrasilomandorinelviorbresthalianorimandrilostarventhionpharostilmorandivoltrenphoraxisthalen");
+        driver.findElement(By.id("displayName")).sendKeys("rasossLasas");
+        driver.findElement(By.id("email")).sendKeys("rassa@gmail.com");
+        driver.findElement(By.id("password")).sendKeys("RasaRasiene123!");
+        driver.findElement(By.id("repeat-password")).sendKeys("RasaRasiene123!");
+        driver.findElement(By.id("dateOfBirth")).sendKeys("1980-09-25");
+        driver.findElement(By.id("other")).click();
+        driver.findElement(By.id("country")).sendKeys("Lithuania");
+        driver.findElement(By.id("privacy-policy")).click();
+
+        WebElement submitButton = driver.findElement(By.xpath("//button[text()='Submit']"));
+
+        ((JavascriptExecutor) driver).executeScript("arguments[0].scrollIntoView(true);", submitButton);
+        sleep(2000);
+
+        ((JavascriptExecutor) driver).executeScript("arguments[0].click();", submitButton);
+
+        sleep(5000);
+
+        WebElement errorMessage = driver.findElement(By.cssSelector("div.text-danger"));
+        assertEquals("Maximum symbols: 100", errorMessage.getText(), "Error message should match");
+    }
 }
