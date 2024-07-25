89d96aab1067910f32710f637be871d88774d98b
REGINA_GREIBUVIENE <r.greibuviene@gmail.com>
2024-07-25 15:36:28 +0300

    Add Registration test with too long Email

--- Selenium/src/test/java/SeleniumTest.java
+++ Selenium/src/test/java/SeleniumTest.java
@@ -511,4 +511,30 @@ public void testRegistrationWithFirstNameEmptyField() throws InterruptedExceptio
         WebElement errorMessage = driver.findElement(By.cssSelector("div.text-danger"));
         assertEquals("May only contain English letters, all lowercase. Can contain numbers, and these symbols ._-", errorMessage.getText(), "Error message should match");
     }
+    @Test
+    public void testRegistrationWithTooLongEmail() throws InterruptedException {
+        driver.findElement(By.cssSelector("#navbarSupportedContent > ul > li:nth-child(2) > a")).click();
+        driver.findElement(By.id("first-name")).sendKeys("Rasa");
+        driver.findElement(By.id("last-name")).sendKeys("Rasiene");
+        driver.findElement(By.id("displayName")).sendKeys("rasossLasas");
+        driver.findElement(By.id("email")).sendKeys("averylongemailaddressconsistingofmanylettersandnumbers1234567890abcdefghijklmnopqrstuvwx@exampledomainwithaverylongnamethatexceedstheusualcharacterlimitforanemailaddress1234567890abcdefghijklmnopqrstuvwxy.com");
+        driver.findElement(By.id("password")).sendKeys("RasaRasiene123!");
+        driver.findElement(By.id("repeat-password")).sendKeys("RasaRasiene123!");
+        driver.findElement(By.id("dateOfBirth")).sendKeys("001980-09-25");
+        driver.findElement(By.id("other")).click();
+
+        WebElement submitButton = driver.findElement(By.xpath("//button[text()='Submit']"));
+        ((JavascriptExecutor) driver).executeScript("arguments[0].scrollIntoView(true);", submitButton);
+        sleep(2000);
+
+        driver.findElement(By.id("country")).sendKeys("Lithuania");
+        driver.findElement(By.id("privacy-policy")).click();
+
+        ((JavascriptExecutor) driver).executeScript("arguments[0].click();", submitButton);
+
+        sleep(5000);
+
+        WebElement errorMessage = driver.findElement(By.cssSelector("div.text-danger"));
+        assertEquals("Maximum length 200 characters", errorMessage.getText(), "Error message should match");
+    }
 }
